c99d8a7296810b0ee5d190d8b29c05e4538e6fb1
phongtran2309 <phongtran99932@gmail.com>
2022-07-11 22:57:23 +0700

    Add input a new task

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,50 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Todo list',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Todo list')),
+        body: const TodoListContainer()),
+    );
+  }
+}
+class TodoListContainer extends StatelessWidget {
+  const TodoListContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _buildHeader(),
+      ],
+    );
+  }
+
+  Widget _buildHeader() {
+    var txtTodoController = TextEditingController();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: TextFormField(
+           controller: txtTodoController,
+           decoration: const InputDecoration(labelText: 'Add todo', hintText: 'Add todo'),
+          ),
+        ),
+        ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+          label: const Text('Add'),
+        ),
+      ],
     );
   }
 }
